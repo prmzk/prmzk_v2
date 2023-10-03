eb94d47dec3c9183fb8680dd946f998fce8c18dd
Adam Primarizki <adam.primarizki@gmail.com>
2023-10-03 20:53:33 +0700

    configure tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      white: "#fffcf2",
+      gray: "#94a3b8",
+      darkGray: "#403d39",
+      black: "#252422",
+      blue: "#00a8e8",
+    },
+    extend: {
+      height: {
+        screen: ["100vh /* fallback for Opera, IE and etc. */", "100dvh"],
+      },
+      minHeight: {
+        screen: ["100vh /* fallback for Opera, IE and etc. */", "100dvh"],
+      },
+    },
+  },
+  plugins: [],
+};
